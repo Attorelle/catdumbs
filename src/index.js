4f649aa7f9a09fc1694834a68dcb3ed92b91e681
Ivan K <attoscape@gmail.com>
2023-02-02 22:34:40 +0300

    src/index.js: implement «forward to admin»

--- src/index.js
+++ src/index.js
@@ -2,10 +2,35 @@ const config = require('config');
 const { Telegraf } = require('telegraf');
 
 const token = config.get('config.token');
+const admin = config.get('config.admin');
 const bot = new Telegraf(token);
 
-bot.start((ctx) => {
-    ctx.reply("Hello there! 🐈‍⬛");
+let isAdmin = (userId) => {
+	return userId == admin;
+};
+
+let forwardToAdmin = async (ctx) => {
+	if (isAdmin(ctx.message.from.id)) {
+		await ctx.deleteMessage();
+	} else {
+		await ctx.forwardMessage(admin, ctx.from.id, ctx.message.id);
+		await ctx.reply("Сообщение отправлено!", {
+			reply_to_message_id: ctx.message.message_id
+		});
+	}
+};
+
+let onStart = {
+	"toUser": "Привет! Это — бот-предложка канала «шерстяные презервативы»! 🐈‍⬛\nНа данный момент принимаются исключительно изображения.",
+	"toAdmin": "Привет! Я готов принимать новые посты!"
+};
+
+bot.start(async (ctx) => {
+    await ctx.reply(isAdmin(ctx.message.from.id) ? onStart.toAdmin : onStart.toUser);
+});
+
+bot.on('message', async (ctx) => {
+	forwardToAdmin(ctx);
 });
 
 bot.launch();
